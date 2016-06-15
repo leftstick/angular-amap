c9fd10b1008dd28f2508922b7159baef1eb34866
Howard.Zuo <leftstick@qq.com>
2016-06-15 12:51:46 +0800

    update demo

--- src/directiveDef.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-
-export const def = function(name, dirName, ddo) {
-    angular.module(name, [])
-        .directive(dirName, [function() {
-            return ddo;
-        }]);
-};
